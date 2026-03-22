07bc1a7bdf26277bea76a4d6993720d7920e9593
GamingDerp <46617199+GamingDerp@users.noreply.github.com>
2026-03-22 15:51:40 -0500

    Update highlightcog.py

--- Cogs/highlightcog.py
+++ Cogs/highlightcog.py
@@ -259,50 +259,60 @@ class HighlightCog(commands.Cog):
                 await ctx.send("You don't have the required permissions for this command!", ephemeral=True)
                 return
             e = discord.Embed(title=f"⚙️ Highlight Help ⚙️", color=commie_color)
-            e.description = "Here are the available public commands for managing highlights. \n### ❓ What does Highlights do ❓ \n'Highlight' commands help with moderation! Each time a word in your 'highlight list' is mentioned, it will **DM** you with information about the user and a link to the message! \n\n### 📌 Highlight Commands 📌 \n> ⚙️ `/highlight help` **|** Sends the Highlight Help Menu \n> 📑 `/highlight defaults` **|** Adds a default list of slurs to your highlight list \n> ➕ `/highlight add [word]` **|** Adds a word to your highlight list \n> ➖ `/highlight remove [word]` **|** Removes a word from your highlight list \n> ♻️ `/highlight clear` **|** Clears your highlight list \n> 🔕 `/highlight ignore [word]` **|** Ignores a word that contains a word in your highlight list **Ex:** If yuou ignore '**hi**', you won't get DM'd when someone says '**hit**'. \n> 🔔 `/highlight unignore [word]` **|** Unignores a word that contains a word in your highlight list \n> ❌ `/highlight block [item]` **|** [`item` can either be a channel mention or user mention], when a user or channel is 'blocked' you won't get DM'd when they say a word in your highlight list \n> ✅ `/highlight unblock [item]` **|** Unblocks a user or channel you had blocked [`item` can either be a channel mention or user mention]\n> 📰 `/highlight show` **|** Shows all the information about your highlight list"
+            e.description = "Here are the available public commands for managing highlights. \n### ❓ What does Highlights do ❓ \n'Highlight' commands help with moderation! Each time a word in your 'highlight list' is mentioned, it will **DM** you with information about the user and a link to the message! \n\n### 📌 Highlight Commands 📌 \n> ⚙️ `/highlight help` **|** Sends the Highlight Help Menu \n> 📑 `/highlight defaults` **|** Adds a default list of slurs to your highlight list \n> ➕ `/highlight add [word]` **|** Adds a word to your highlight list \n> ➖ `/highlight remove [word]` **|** Removes a word from your highlight list \n> ♻️ `/highlight clear` **|** Clears your highlight list \n> 🔕 `/highlight ignore [word]` **|** Ignores a word that contains a word in your highlight list **Ex:** If you ignore '**hi**', you won't get DM'd when someone says '**hit**'. \n> 🔔 `/highlight unignore [word]` **|** Unignores a word that contains a word in your highlight list \n> ❌ `/highlight block [item]` **|** [`item` can either be a channel mention or user mention], when a user or channel is 'blocked' you won't get DM'd when they say a word in your highlight list \n> ✅ `/highlight unblock [item]` **|** Unblocks a user or channel you had blocked [`item` can either be a channel mention or user mention]\n> 📰 `/highlight show` **|** Shows all the information about your highlight list"
             await ctx.send(embed=e, ephemeral=True)
         except Exception as e:
             print(e)
 
+    def check_for_access(self, member: discord.Member, channel: discord.abc.GuildChannel) -> bool:
+        try:
+            if not member or not channel:
+                return
+            if isinstance(channel, discord.Thread):
+                channel = channel.parent
+            permissions = channel.permissions_for(member)
+            return permissions.view_channel
+        except Exception as e:
+            print(f"Access check failed: {e}")
+
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.author.bot:
-            return
-        async with aiosqlite.connect("dbs/highlight.db") as db:
-            async with db.execute("SELECT user_id FROM user_highlights WHERE server_id = ?", (message.guild.id,)) as cursor:
-                user_ids = await cursor.fetchall()
-        for user_id in user_ids:
-            user_id = user_id[0]
-            word_list, ignored_words_list, ignored_channels, ignored_users = await self.get_user_data(user_id, message.guild.id)
-            words = word_list.split(',') if word_list else []
-            ignored_words = ignored_words_list.split(',') if ignored_words_list else []
-            ignored_channel_ids = [int(cid) for cid in ignored_channels.split(',')] if ignored_channels else []
-            ignored_user_ids = [int(uid) for uid in ignored_users.split(',')] if ignored_users else []
-            if message.channel.id in ignored_channel_ids or message.author.id in ignored_user_ids:
-                continue
-            if message.author.id == user_id:
-                continue
-            user = self.bot.get_user(user_id)
-            if f"<@{user_id}>" in message.content:
-                continue
-            content = message.clean_content.lower()
-            for word in words:
-                if word in content and not any(ignored_word in content for ignored_word in ignored_words):
-                    if user:
-                        e = discord.Embed(
-                            title=f"🚨 Word Mentioned 🚨",
-                            color=commie_color,
-                            timestamp=datetime.utcnow()
-                        )
-                        e.set_author(name=message.guild.name, icon_url=message.guild.icon.url)
-                        e.set_thumbnail(url=message.author.avatar.url)
-                        e.add_field(name="🔍 Mentioned Word", value=f"> {word}", inline=False)
-                        e.add_field(name="💬 Message", value=f"> {message.clean_content}", inline=False)
-                        e.add_field(name="👤 Mentioned By", value=f"> {message.author.mention}", inline=False)
-                        e.add_field(name="📢 In Channel", value=f"> {message.channel.mention}", inline=False)
-                        e.add_field(name="🔗 Jump Link", value=f"> [Message]({message.jump_url})", inline=False)
-                        await user.send(embed=e)
-                    break
+        try:
+            if message.author.bot:
+                return
+            async with aiosqlite.connect("dbs/highlight.db") as db:
+                async with db.execute("SELECT user_id FROM user_highlights WHERE server_id = ?", (message.guild.id,)) as cursor:
+                    user_ids = await cursor.fetchall()
+            for user_id in user_ids:
+                user_id = user_id[0]
+                word_list, ignored_words_list, ignored_channels, ignored_users = await self.get_user_data(user_id, message.guild.id)
+                words = word_list.split(',') if word_list else []
+                ignored_words = ignored_words_list.split(',') if ignored_words_list else []
+                ignored_channel_ids = [int(cid) for cid in ignored_channels.split(',')] if ignored_channels else []
+                ignored_user_ids = [int(uid) for uid in ignored_users.split(',')] if ignored_users else []
+                if message.channel.id in ignored_channel_ids or message.author.id in ignored_user_ids:
+                    continue
+                if message.author.id == user_id:
+                    continue
+                user = self.bot.get_user(user_id)
+                if f"<@{user_id}>" in message.content:
+                    continue
+                content = message.clean_content.lower()
+                for word in words:
+                    if word in content and not any(ignored_word in content for ignored_word in ignored_words):
+                        if member and self.check_for_access(member, message.channel):
+                            e = discord.Embed(title=f"🚨 Word Mentioned 🚨", color=commie_color, timestamp=datetime.utcnow())
+                            e.set_author(name=message.guild.name, icon_url=message.guild.icon.url)
+                            e.set_thumbnail(url=message.author.avatar.url)
+                            e.add_field(name="🔍 Mentioned Word", value=f"> {word}", inline=False)
+                            e.add_field(name="💬 Message", value=f"> {message.clean_content}", inline=False)
+                            e.add_field(name="👤 Mentioned By", value=f"> {message.author.mention}", inline=False)
+                            e.add_field(name="📢 In Channel", value=f"> {message.channel.mention}", inline=False)
+                            e.add_field(name="🔗 Jump Link", value=f"> [Message]({message.jump_url})", inline=False)
+                            await member.send(embed=e)
+                        break
+        except Exception as e:
+            print(e)
 
 async def setup(bot):
     await bot.add_cog(HighlightCog(bot))
